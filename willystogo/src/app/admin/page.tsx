89a6fba0e1688dd11c2ad87430e733749785bf0a
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-06 20:16:53 +0200

    admin page

--- willystogo/src/app/admin/page.tsx
+++ willystogo/src/app/admin/page.tsx
@@ -2,17 +2,6 @@
 
 import { redirect } from "next/navigation"
 import { useSession } from "next-auth/react"
-import { cache, use } from "react"
-
-type User = {
-  id: number
-  name: string
-  email: string
-}
-
-const getUsers = cache(() =>
-  fetch("https://jsonplaceholder.typicode.com/users").then((res) => res.json())
-)
 
 export default function Profile() {
   const { status } = useSession({
@@ -26,31 +15,11 @@ export default function Profile() {
     return <p>Loading....</p>
   }
 
-  let users = use<User[]>(getUsers())
 
   return (
-    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr 1fr",
-          gap: 20,
-        }}
-      >
-        {users.map((user) => (
-          <div
-            key={user.id}
-            style={{ border: "1px solid #ccc", textAlign: "center" }}
-          >
-            <img
-              src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
-              alt={user.name}
-              style={{ height: 180, width: 180 }}
-            />
-            <h3>{user.name}</h3>
-          </div>
-        ))}
-      </div>
-    </main>
+    <div className="flex flex-col items-center justify-center py-2">
+        <h1>Admin</h1>
+        <p>Only accessible to authenticated users</p>
+    </div>
   )
 }
