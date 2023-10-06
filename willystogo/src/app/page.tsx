9086b8f467c7c2dca9acdfeca683f99f25553d43
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-06 21:05:29 +0200

    install tiptap

--- willystogo/src/app/page.tsx
+++ willystogo/src/app/page.tsx
@@ -6,12 +6,23 @@ import {
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
 import { User } from "@/app/components/user"
+import Image from 'next/image'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
+      
+      <div>
+        <Image
+          src="/logo.png"
+          alt="Willy's To Go"
+          width={200}
+          height={200}
+        />
+      </div>
+
       <div className="flex flex-row items-center justify-center space-x-4">
         <LoginButton />
         <LogoutButton />
