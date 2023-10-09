dfd09015e8bdc5909c60e1f565e20fd35166f936
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-09 17:12:19 +0200

    small admin changes

--- willystogo/src/app/admin/page.tsx
+++ willystogo/src/app/admin/page.tsx
@@ -6,21 +6,21 @@ const Profile = async () => {
 
     const session = await getServerSession(authOptions)
 
-    // if (!session) {
-    //     redirect("/api/auth/signin?callbackUrl=/admin")
-    // }
+    if (!session) {
+        redirect("/api/auth/signin?callbackUrl=/admin")
+    }
 
     return (
-        <section className='py-24'>
-            <div className='container'>
-                <h1 className='text-2xl font-bold'>
-                    This is a <span className='text-emerald-500'>server-side</span>{' '}
-                    protected page
-                </h1>
-                <h2 className='mt-4 font-medium'>You are logged in as:</h2>
-                <p className='mt-4'>{session?.user?.name}</p>
+            <div className='flex flex-col items-center justify-center p-2'>
+                <div className='flex flex-col w-full flex-1 px-20'>
+                    <h1 className='text-2xl font-bold'>
+                        This is a <span className='text-emerald-500'>server-side</span>{' '}
+                        protected page
+                    </h1>
+                    <h2 className='mt-4 font-medium'>You are logged in as:</h2>
+                    <p className='mt-4'>{session?.user?.name}</p>
+                </div>
             </div>
-        </section>
         )
     }
     
