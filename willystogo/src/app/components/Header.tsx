e953b5ff446116bc683efb38bafd82695d76376e
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-15 17:42:40 +0200

    header

--- willystogo/src/app/components/Header.tsx
+++ willystogo/src/app/components/Header.tsx
@@ -1,20 +1,18 @@
 "use client"
 
-import React from 'react'
-import Container from '@/app/components/ui/container'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useSession } from 'next-auth/react'
+import React, { useState, useEffect } from 'react';
+import Container from '@/app/components/ui/container';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
-import { Button } from '@/app/components/ui/button'
-import { AdminModeWithLogout } from "@/app/components/admin/headerButton"
-import { Sheet, SheetContent, SheetTrigger } from '@/app/components/ui/sheet'
-import { Menu } from 'lucide-react'
-
+import { Button } from '@/app/components/ui/button';
+import { AdminModeWithLogout } from "@/app/components/admin/headerButton";
+import { Sheet, SheetContent, SheetTrigger } from '@/app/components/ui/sheet';
+import { Menu } from 'lucide-react';
 
 const Header = () => {
-
-  const { data: session } = useSession()  
+  const { data: session } = useSession();
 
   const routes = [
     {
@@ -25,42 +23,50 @@ const Header = () => {
       href: "/satebar",
       label: "Satebar",
     },
-  ]
+  ];
+
+  const [position, setPosition] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let moving = window.scrollY;
+      setVisible(position > moving);
+      setPosition(moving);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const headerClass = visible ? 'top-0 transition-all duration-400 ease-out bg-primary z-50' : 'bg-primary -top-24 transition-all duration-400 ease-out z-50';
 
   return (
-    <header className='sm:flex sm:justify-between py-3 px-4 border-b bg-primary font-youngSerif'>
+    <header className={`fixed w-full p-8 bg-lime flex justify-center ${headerClass}`}>
       <Container>
-        <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full'>
+        <div className='relative px-4 sm:px-6 lg:px-8 flex h-6 items-center justify-between w-full'>
           <div className='flex items-center'>
-            {/* <Link href='/'>
-                <Image 
-                    src='/logo.png' 
-                    alt='Logo'
-                    width={200}
-                    height={200}
-                    priority
-                />
-            </Link> */}
             <h1 className='text-3xl font-youngSerif text-white'>YanWilly</h1>
           </div>
           <Sheet>
-              <SheetTrigger>
-                <Menu className="h-6 md:hidden" />
-              </SheetTrigger>
-              <SheetContent side="right" className='w-[300px] sm:w-[400px]'>
-                <nav className='flex flex-col gap-4'>
-                  {routes.map((route, i) => (
-                    <Link 
-                      key={i}
-                      href={route.href}
-                      className='block px-4 py-1 text-3xl font-youngSerif'
-                    >
-                      {route.label}
-                    </Link>
-                  ))}
-                </nav>
-              </SheetContent>
-            </Sheet>
+            <SheetTrigger>
+              <Menu className="h-6 md:hidden" />
+            </SheetTrigger>
+            <SheetContent side="right" className='w-[300px] sm:w-[400px]'>
+              <nav className='flex flex-col gap-4'>
+                {routes.map((route, i) => (
+                  <Link 
+                    key={i}
+                    href={route.href}
+                    className='block px-4 py-1 text-3xl font-youngSerif'
+                  >
+                    {route.label}
+                  </Link>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
           <div className=''>
             <nav className='mx-6 items-center space-x-4 lg:space-x-6 hidden md:block'>
               {routes.map((route, i) => (
@@ -87,7 +93,7 @@ const Header = () => {
         </div>
       </Container>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
