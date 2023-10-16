8266ecab6ab42ef533c475fa5bfcb747cc962634
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-16 15:56:48 +0200

    updated env variables

--- willystogo/src/app/api/content/route.ts
+++ willystogo/src/app/api/content/route.ts
@@ -1,50 +1,71 @@
-import { getParagraphJson } from '@/app/utils/db';
-import { MongoClient, Db } from 'mongodb';
-import { NextResponse } from 'next/server';
+// import { getParagraphJson } from '@/app/utils/db';
+// import { MongoClient, Db } from 'mongodb';
+// import { NextResponse } from 'next/server';
 
-const uri: string | undefined = process.env.MONGODB_URI;
-if (!uri) throw new Error("The MONGODB_URI environment variable must be defined.");
+// const uri: string | undefined = process.env.MONGODB_URI;
+// if (!uri) throw new Error("The MONGODB_URI environment variable must be defined.");
 
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: '1',
-        strict: true,
-        deprecationErrors: true,
-    },
-});
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: '1',
+//         strict: true,
+//         deprecationErrors: true,
+//     },
+// });
 
-let cachedDb: Db | undefined;
+// let cachedDb: Db | undefined;
 
-// Function to connect to the database
-async function connectToDatabase(): Promise<Db> {
-    if (cachedDb) {
-        return cachedDb;
-    }
+// // Function to connect to the database
+// async function connectToDatabase(): Promise<Db> {
+//     if (cachedDb) {
+//         return cachedDb;
+//     }
 
-    // Connect the client to the server
-    await client.connect();
+//     // Connect the client to the server
+//     await client.connect();
 
-    const dbName: string | undefined = process.env.MONGODB_DB;
-    if (!dbName) throw new Error("The MONGODB_DB environment variable must be defined.");
+//     const dbName: string | undefined = process.env.MONGODB_DB;
+//     if (!dbName) throw new Error("The MONGODB_DB environment variable must be defined.");
 
-    const db = client.db(dbName);
-    cachedDb = db;
-    return db;
-}
+//     const db = client.db(dbName);
+//     cachedDb = db;
+//     return db;
+// }
+
+// // Get a paragraph 
+// export async function GET(request: Request) {
+//     await connectToDatabase();
+
+//     const documentId = request.headers.get("Document-ID");
+//     if (!documentId) {
+//         return NextResponse.json({ error: "Document-ID header is missing." }, { status: 400 });
+//     }
+
+//     const result = await getParagraphJson(documentId);
+//     console.log("Get request:", result);
+
+//     return new Response(JSON.stringify(result), {
+//         headers: { "Content-Type": "application/json" },
+//     });
+// }
+
+import { NextResponse } from "next/server";
+import { getParagraphJson } from "@/app/utils/db";
 
 // Get a paragraph 
 export async function GET(request: Request) {
-    await connectToDatabase();
-
     const documentId = request.headers.get("Document-ID");
+
     if (!documentId) {
         return NextResponse.json({ error: "Document-ID header is missing." }, { status: 400 });
     }
 
     const result = await getParagraphJson(documentId);
+
     console.log("Get request:", result);
 
     return new Response(JSON.stringify(result), {
         headers: { "Content-Type": "application/json" },
     });
 }
+
