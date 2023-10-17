5959de9ac177cb8dd94ff5d190cbdb34b3a49b4c
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-17 11:22:08 +0200

    header nav

--- willystogo/src/app/components/Header.tsx
+++ willystogo/src/app/components/Header.tsx
@@ -41,34 +41,51 @@ const Header = () => {
     };
   });
 
-  const headerClass = visible ? 'top-0 transition-all duration-400 ease-out bg-primary z-50' : 'bg-primary -top-24 transition-all duration-400 ease-out z-50';
+  const headerClass = visible ? 'top-0 transition-all duration-400 ease-out z-50 bg-primary' : 'bg-primary -top-24 transition-all duration-400 ease-out z-50';
 
   return (
     <header className={`sticky w-full p-8 flex justify-center ${headerClass}`}>
       <Container>
-        <div className='relative px-4 sm:px-6 lg:px-8 flex h-6 items-center justify-between w-full'>
+        <div className='relative pr-4 sm:px-6 lg:px-8 flex h-6 items-center justify-between w-full'>
+          
+          {/* Logo */}
+          <Link href='/' className='-ml-6'>
+            {/* <div className='flex items-center'>
+              <h1 className='text-3xl font-youngSerif text-white'>YanWilly</h1>
+            </div> */}
+            <Image
+              src='/logo.png'
+              alt='Logo'
+              width={160}
+              height={100}
+              priority
+            />
+          </Link>
+          
+          {/* Navigation and Session Content */}
           <div className='flex items-center'>
-            <h1 className='text-3xl font-youngSerif text-white'>YanWilly</h1>
-          </div>
-          <Sheet>
-            <SheetTrigger>
-              <Menu className="h-6 md:hidden invert" />
-            </SheetTrigger>
-            <SheetContent side="right" className='w-[300px] sm:w-[400px]'>
-              <nav className='flex flex-col gap-4'>
-                {routes.map((route, i) => (
-                  <Link 
-                    key={i}
-                    href={route.href}
-                    className='block px-4 py-1 text-3xl font-youngSerif'
-                  >
-                    {route.label}
-                  </Link>
-                ))}
-              </nav>
-            </SheetContent>
-          </Sheet>
-          <div className=''>
+            
+            {/* Mobile Navigation (SheetTrigger) */}
+            <Sheet>
+              <SheetTrigger>
+                <Menu className="h-6 md:hidden invert" />
+              </SheetTrigger>
+              <SheetContent side="right" className='w-[300px] sm:w-[400px]'>
+                <nav className='flex flex-col gap-4'>
+                  {routes.map((route, i) => (
+                    <Link 
+                      key={i}
+                      href={route.href}
+                      className='block px-4 py-1 text-3xl font-youngSerif'
+                    >
+                      {route.label}
+                    </Link>
+                  ))}
+                </nav>
+              </SheetContent>
+            </Sheet>
+            
+            {/* Desktop Navigation */}
             <nav className='mx-6 items-center space-x-4 lg:space-x-6 hidden md:block'>
               {routes.map((route, i) => (
                 <Button key={i}>
@@ -85,16 +102,19 @@ const Header = () => {
               ))}
             </nav>
             
+            {/* Session Content */}
             {session && (
               <div className='flex items-center'>
                 <AdminModeWithLogout />
               </div>
             )}
-          </div>
+  
+          </div> {/* End of Navigation and Session Content */}
+          
         </div>
       </Container>
     </header>
   );
-}
+}  
 
 export default Header;
