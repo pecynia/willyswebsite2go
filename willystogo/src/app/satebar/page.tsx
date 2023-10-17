eca927183613f56c2e54f15b13feedce45ce6df2
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-17 11:43:10 +0200

    in ontwikkeling

--- willystogo/src/app/satebar/page.tsx
+++ willystogo/src/app/satebar/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { ReloadIcon } from "@radix-ui/react-icons"
+
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="flex justify-center items-center h-screen font-youngSerif text-5xl">
+      In ontwikkeling...
+      <ReloadIcon className="ml-4 mr-2 h-8 w-8 animate-spin" />
+    </div>
   )
 }
 
