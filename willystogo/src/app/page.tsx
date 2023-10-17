02456eccb97f577c479ac87db7aaae5dafebc021
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-17 11:07:26 +0200

    wrapper text

--- willystogo/src/app/page.tsx
+++ willystogo/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <svg className='' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="var(--svg)" fillOpacity="1" d="M0,128L48,154.7C96,181,192,235,288,245.3C384,256,480,224,576,208C672,192,768,192,864,197.3C960,203,1056,213,1152,197.3C1248,181,1344,139,1392,117.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> 
       </div>
 
-      <div className=' -mb-1 md:-mt-24 lg:-mt-42 z-20 overflow-hidden block sm:hidden bg-secondary rounded-t-3xl'>
+      <div className='pt-4 px-2 -mb-1 md:-mt-24 lg:-mt-42 z-20 overflow-hidden block sm:hidden bg-secondary rounded-t-3xl'>
         <EditorWrapper documentId="intro"/> 
       </div>
       <div className='h-16 -mt-10 bg-primary overflow-hidden rounded-t-3xl'></div>
