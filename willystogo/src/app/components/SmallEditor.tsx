d47f63bb5c0feb0152ed99498bf28f9dda2f0b95
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-18 18:46:50 +0200

    Goeie textleftimgright text component

--- willystogo/src/app/components/SmallEditor.tsx
+++ willystogo/src/app/components/SmallEditor.tsx
@@ -6,7 +6,7 @@ function SmallEditor({documentId}: {documentId: string}) {
   return (
     <div>
         <div className='pt-4 px-2 -mb-1 -mt-20 md:-mt-24 lg:-mt-42 z-20 overflow-hidden block md:hidden bg-secondary'>
-            <EditorWrapper documentId="intro"/> 
+            <EditorWrapper documentId={documentId} />
       </div>
     </div>
   )
--- willystogo/src/app/components/TextLeftImgRight.tsx
+++ willystogo/src/app/components/TextLeftImgRight.tsx
@@ -46,20 +46,21 @@ const TextLeftImgRight: React.FC<TextLeftImgRightProps> = ({ documentId, images 
   }, [])
 
   return (
-    <div ref={container} className="mt-0 sm:mt-10 pl-8 sm:pl-24 h-[500px] flex justify-start p-4 sm:p-16 items-center">
-      <div className="header-shadow-left w-full md:w-3/5 lg:w-2/5 sm:h-full md:h-[140%] mt-0 md:mt-24 lg:h-full bg-secondary flex px-10 pt-4">
+    <div ref={container} className="mt-0 sm:mt-10 pl-8 sm:pl-24 min-h-[500px] grid grid-cols-1 md:grid-cols-5 gap-4 p-4 sm:p-16">
+      <div className="header-shadow-left col-span-3 bg-secondary flex px-10 pt-4">
         <EditorWrapper documentId={documentId} />
       </div>
-      <div className="relative w-2/5 h-full flex-shrink-0 pl-2 md:ml-12 lg:pl-8 hidden md:block">
-        <motion.div style={{ y: useTransform(scrollYProgress, [0, 1], [0, -dimension.height * 0.2]) }} className="absolute top-[40%] ml-0 lg:ml-8 w-[100%] h-[70%] md:w-[100%] md:h-[90%] lg:w-[80%] lg:h-[100%]">
-          <Image src={images[0]} alt="Image 1" fill className="object-cover object-center" />
+      <div className="relative col-span-2 flex flex-col justify-between">
+        <motion.div style={{ y: useTransform(scrollYProgress, [0, 1], [0, -dimension.height * 0.2]) }} className="w-full mt-24 h-[60%]">
+          <Image src={images[0]} alt="Image 1" layout="fill" className="object-cover object-center" />
         </motion.div>
-        <motion.div style={{ y: useTransform(scrollYProgress, [0, 1], [0, dimension.height * 0.2]) }} className="absolute ml-16 md:ml-20 lg:ml-48 w-[70%] h-[50%] md:w-[80%] md:h-[60%] lg:w-[70%] lg:h-[70%]">
-          <Image src={images[1]} alt="Image 2" fill className="object-cover object-center" />
+        <motion.div style={{ y: useTransform(scrollYProgress, [0, 1], [0, dimension.height * 0.2]) }} className="w-full h-[60%]">
+          <Image src={images[1]} alt="Image 2" layout="fill" className="object-cover object-center" />
         </motion.div>
       </div>
     </div>
-  )
+)
+
 }
 
 export default TextLeftImgRight
