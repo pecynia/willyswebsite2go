29c23b3f4001eade4118d6dfe855178203ea54b3
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-19 13:51:16 +0200

    adjusted admin dashboard

--- willystogo/src/app/api/auth/signin/page.tsx
+++ willystogo/src/app/api/auth/signin/page.tsx
@@ -45,22 +45,17 @@ const SignInPage: NextPage = () => {
   return (
       <section className='flex min-h-full overflow-hidden pt-16 sm:py-28'>
           <div className='mx-auto flex w-full max-w-2xl flex-col px-4 sm:px-6'>
-              <div className='relative mt-12 sm:mt-16'>
-                  <h1 className='text-center text-2xl font-medium tracking-tight text-gray-900'>
-                      Log in bij het admin dashboard
-                  </h1>
-              </div>
-              <div className='sm:rounded-5xl -mx-4 flex-auto bg-white px-4 py- shadow-2xl shadow-gray-900/10 sm:mx-0 sm:flex-none sm:p-24'>
+              <div className='sm:rounded-5xl -mx-4 flex-auto bg-white px-4 header-shadow-right sm:mx-0 sm:flex-none sm:p-10'>
                   <form onSubmit={handleSubmit(handleFormSubmit)}>
                       <div className='space-y-2'>
-                          <label htmlFor='password' className='block text-sm font-medium text-gray-700'>
+                          <label htmlFor='password' className='block text-md font-medium text-gray-700'>
                               Wachtwoord
                           </label>
                           <Input
                               id='password'
                               type='password'
                               required
-                              className='mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md'
+                              className='mt-1 block w-full text-md py-2 px-3 border border-gray-300 rounded-md'
                               placeholder='Vul je wachtwoord in'
                               {...register('password')}
                               autoFocus
@@ -71,7 +66,7 @@ const SignInPage: NextPage = () => {
                       </div>
                       <Button
                           type='submit'
-                          className='mt-3 w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'
+                          className='mt-3 w-full  py-2 px-4 rounded'
                       >
                         <LogIn className="h-5 w-5 mr-2" />
                         Log In
