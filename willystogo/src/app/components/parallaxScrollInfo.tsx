ff879968f9fce107797ee3a865f5ffb4c38d1489
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-10-19 14:02:53 +0200

    kinda grows

--- willystogo/src/app/components/parallaxScrollInfo.tsx
+++ willystogo/src/app/components/parallaxScrollInfo.tsx
@@ -29,12 +29,13 @@ interface TextboxProps {
 
 const Textbox: React.FC<TextboxProps> = ({ y, isActive }) => (
   <motion.div 
-    className={`header-shadow-right absolute right-0 top-20 mr-12 my-4 w-2/5 h-2/5 mb-12 bg-secondary p-4 hidden lg:block ${isActive ? 'fixed' : ''}`}
+    className={`header-shadow-right absolute right-0 top-0 mr-12 my-4 w-2/5 min-h-[20%] max-h-[80%] mb-12 bg-secondary p-4 overflow-y-auto hidden lg:block ${isActive ? 'fixed' : ''}`}
     style={{ y }}
   >
       <EditorWrapper documentId="intro"/>
   </motion.div>
 )
+
 
 
 const Column: React.FC<ColumnProps> = ({ images, y, positionClass }) => (
@@ -107,9 +108,9 @@ const ParallaxScrollInfo = () => {
 
   return (
     <div>
-      <div className="-mb-1  h-[110vh] sm:h-[125vh] lg:h-[170vh] overflow-hidden bg-cover bg-center bg-primary-secondary-foreground">
-        <div ref={gallery} className="h-[175vh] overflow-hidden bg-cover bg-center relative z-10">
-          <div className="relative -top-12 h-[200vh] flex space-x-4 px-4">
+      <div className="-mb-1 min-h-[110vh] sm:min-h-[125vh] lg:min-h-[150vh] overflow-hidden bg-cover bg-center bg-primary-secondary-foreground">
+      <div ref={gallery} className="h-[175vh] bg-cover bg-center relative z-10 pb-32 lg:pb-1/3">
+          <div className="relative -top-12 h-[250vh] flex space-x-4 px-4">
             <Column images={[images[0], images[4], images[2]]} y={y} positionClass="top-[-40%]" />
             <Column images={[images[3], images[1], images[5]]} y={y2} positionClass="top-[-90%] sm:block hidden" />
           </div>
