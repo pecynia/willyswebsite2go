8e0d91fbb11a703f1b6b0b51009c576c5f576031
Nicolai Verheul <n.verheul@student.science.ru.nl>
2023-11-10 15:21:03 +0100

    Looks good

--- willystogo/src/app/components/OtherReviews.tsx
+++ willystogo/src/app/components/OtherReviews.tsx
@@ -4,10 +4,9 @@ import { motion } from 'framer-motion';
 
 import { reviews } from '@/dictionaries/reviews';
 
-
 function OtherReviews() {
   return (
-    <div className="">
+    <div className="p-8">
         {reviews.quotes.map((review, index) => (
           <motion.div 
             key={index}
@@ -15,36 +14,22 @@ function OtherReviews() {
             whileInView={{ opacity: 1, x: '0%' }}
             viewport={{ once: true }}
             transition={{ duration: 0.7, delay: 0.2, ease: [0, 0.71, 0.2, 1.01] }}
-            className={`flex flex-col md:flex-row items-center max-w-6xl mx-auto m-16 bg-secondary-foreground ${index % 2 === 0 ? 'header-shadow-right ml-[15%]' : 'mr-[15%] header-shadow-left'}`}
+            className={`flex flex-col md:flex-row items-center max-w-4xl mx-auto m-16 bg-secondary-foreground ${index % 2 === 0 ? 'header-shadow-left lg:ml-[15%]' : 'header-shadow-right lg:mr-[15%]'}`}
           >
-            {index % 2 !== 0 && (
-                <div>
-                    <Image
-                        src={review.img.path}
-                        alt={review.img.alt}
-                        width={300}
-                        height={200}
-                        className="object-center object-cover"
-                    />
-              </div>
-            )}
-            <div className="flex-1 px-10 py-12 ">
-                <h3 className="text-2xl font-bold">{review.companyName || review.author}</h3>
-                <p className="italic">{review.date}</p>
-                <p>{review.quote}</p>
-                {review.companyName && <p className="font-bold">{review.author}</p>}
+            <div className={`${index % 2 === 0 ? 'order-2' : ''} w-full md:w-1/4 relative`} style={{ aspectRatio: '1 / 1.5' }}>
+              <Image
+                src={review.img.path}
+                alt={review.img.alt}
+                fill
+                className="object-center object-cover"
+              />
             </div>
-            {index % 2 === 0 && (
-                <div>
-                    <Image
-                        src={review.img.path}
-                        alt={review.img.alt}
-                        width={300}
-                        height={200}
-                        className="object-cover object-center"
-                    />
-                </div>
-            )}
+            <div className="flex-1 px-10 py-12 ">
+              <h3 className="text-2xl font-bold">{review.companyName || review.author}</h3>
+              <p className="italic">{review.date}</p>
+              <p>{review.quote}</p>
+              {review.companyName && <p className="font-bold">{review.author}</p>}
+            </div>
           </motion.div>
         ))}
       </div>
